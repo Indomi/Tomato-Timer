02157238d50c447d7167e50ddb559bb8fb080d42
Indomi <UestcSyy@163.com>
2017-09-24 17:47:01 +0800

    change view
    更改功能点，加入动画

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,71 +7,169 @@ Page({
     animationDataRight:{},
     duration:5000,
     show:true,
+    slideShow:true,
     event:'',
-    lastTime:0,
-    input:0
+    minutes:'00',
+    seconds:'00',
+    input:0,
+    animationSlide:{},
+    animationSlideBtnDown:{},
+    animationSlideBtnShow:{}
   },
-  //绑定倒计时按钮
-  startRotate:function(){
-    var time;
-    var _this = this;
-    var stop = setInterval(function(){
-      let lasttime = _this.data.lastTime;
-      if (lasttime-1 <= 0){
-        clearInterval(stop);
-        console.log("hello");
-        wx.vibrateLong({
-          fail:function(e){console.log(e)},
-          success:function(){console.log('success')}
-        }); 
-      }
-      _this.setData({
-        lastTime:lasttime-1
-      });
-    },1000);
-    if(this.data.lastTime>0){
-      time = this.data.lastTime*500;
-    }else{
-      time = this.data.duration/2;
-    }
+  //进度条动画
+  circleAnimation:function(id){
+    var time=getApp().globalData[id]*30000;
     var animationLeft = wx.createAnimation({
-      duration:time,
-      timingFunction:"linear",
-      delay:time
+      duration: time,
+      timingFunction: "linear",
+      delay: time
     });
     var animationRight = wx.createAnimation({
-      duration:time,
-      timingFunction:"linear",
+      duration: time,
+      timingFunction: "linear",
     });
     this.setData({
-      animationDataLeft:animationLeft.rotate(45).step().export(),
-      animationDataRight:animationRight.rotate(45).step().export(),
+      animationDataLeft: animationLeft.rotate(45).step().export(),
+      animationDataRight: animationRight.rotate(45).step().export(),
       show:false
     });
+    this.slideDown();
   },
-  //重置进度条
-  resetRotate:function(){
-    var animationLeft = wx.createAnimation({
-      duration: 500,
-      timingFunction: "linear",
-      delay: 500
-    });
-    var animationRight = wx.createAnimation({
-      duration: 500,
-      timingFunction: "linear",
-    });
+  //选择休息模式
+  chooseRestTime: function () {
+    //获取全局数据
+    var resttime = getApp().globalData.rest_time;
+    var _this = this;
+    //分钟小于0补0
+    if (resttime < 10) {
+      resttime = '0' + resttime;
+    }
     this.setData({
-      animationDataLeft: animationLeft.rotate(-135).step().export(),
-      animationDataRight: animationRight.rotate(-135).step().export(),
-      show:true,
-      lastTime:this.data.input
+      minutes: resttime
+    });
+    //秒数计时
+    var stop = setInterval(function () {
+      var time = _this.data.seconds;
+      var m = _this.data.minutes;
+      if (time == 0) {
+        _this.setData({
+          seconds: 59
+        });
+        if (m != 0) {
+          m--;
+          if (m < 10) { m = '0' + m; }
+          _this.setData({
+            minutes: m
+          });
+        }
+      } else if (time < 11) {
+        time -= 1;
+        _this.setData({
+          seconds: '0' + time
+        });
+      } else {
+        _this.setData({
+          seconds: time - 1
+        });
+      }
+      //判断结束
+      if (_this.data.minutes == 0 && _this.data.seconds == 0) {
+        clearInterval(stop);
+        _this.stopTimer();
+      }
+    }, 1000);
+    //调用动画
+    this.circleAnimation('rest_time');
+  },
+  //选择工作模式
+  chooseWorkTime: function () {
+    //获取全局数据
+    var worktime = getApp().globalData.work_time;
+    var _this = this;
+    //分钟小于0补0
+    if (worktime < 10) {
+      worktime = '0' + worktime;
+    }
+    this.setData({
+      minutes: worktime
+    });
+    //秒数计时
+    var stop = setInterval(function () {
+      var time = _this.data.seconds;
+      var m = _this.data.minutes;
+      if (time == 0) {
+        _this.setData({
+          seconds: 59
+        });
+        if(m!=0){
+          m--;
+          if(m<10){m='0'+m;}
+          _this.setData({
+            minutes:m
+          });
+        }
+      } else if (time < 11) {
+        time -= 1;
+        _this.setData({
+          seconds: '0' + time
+        });
+      } else {
+        _this.setData({
+          seconds: time - 1
+        });
+      }
+      //判断结束
+      if (_this.data.minutes==0&&_this.data.seconds == 0){
+        clearInterval(stop);
+      }
+    }, 1000);
+    //调用动画
+    this.circleAnimation('work_time');
+  },
+  //运行Timer动画
+  slideDown:function(){
+    var animationSlide = wx.createAnimation({
+      duration:1000,
+      timingFunction:'linear'
+    });
+    animationSlide.height('1100rpx').step();
+    this.setData({
+      animationSlide:animationSlide.export()
+    });
+    animationSlide.opacity(0).step();
+    this.setData({
+      animationSlideBtnDown:animationSlide.export(),
+      slideShow:false
+    });
+    var animation = wx.createAnimation({
+      duration:1000,
+      timingFunction:'linear',
+      delay:1000
+    });
+    animation.opacity(1).step();
+    this.setData({
+      animationSlideBtnShow: animation.export(),
     });
   },
-  //改变持续时间
-  changeDuration:function(e){
+  stopTimer:function(){
+    var animation = wx.createAnimation({
+      duration:1000,
+      timingFunction:'linear'
+    });
+    animation.opacity(0).step();
     this.setData({
-      lastTime:e.detail.value,
-      input:e.detail.value
+      animationSlideBtnShow:animation.export()
+    });
+    animation.opacity(1).step();
+    this.setData({
+      animationSlideBtnDown:animation.export()
+    });
+    animation.height('800rpx').step();
+    this.setData({
+      animationSlide:animation.export(),
+      slideShow:true,
+      minutes:'00',
+      seconds:'00'
     });
   }
 })
--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -1,65 +1,15 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+    rest_time:getApp().globalData.rest_time,
+    work_time:getApp().globalData.work_time
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  //改变时间
+  changeRestTime:function(e){
+    var app = getApp();
+    app.globalData.rest_time = e.detail.value;
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  changeWorkTime: function (e) {
+    var app = getApp();
+    app.globalData.work_time = e.detail.value;
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
